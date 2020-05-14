d0af6fab6e3a19deafc65d58138f28f6a168817c
yangbishi <1110968@greatwall.com.cn>
2020-05-14 13:00:18 +0800

    初次提交

---
+++ src/main/java/com/greatwall/jhgx/mapper/MemberMapper.java
@@ -0,0 +1,10 @@
+package com.greatwall.jhgx.mapper;
+
+import com.greatwall.jhgx.domain.Member;
+import com.greatwall.jhgx.domain.PayOrder;
+
+import java.util.List;
+
+public interface MemberMapper extends SuperMapper<Member> {
+    Member selectMinDayOrderAmtCertId(String today);
+}
---
+++ src/main/java/com/greatwall/jhgx/mapper/PayOrderMapper.java
@@ -0,0 +1,12 @@
+package com.greatwall.jhgx.mapper;
+
+import com.greatwall.jhgx.domain.PayOrder;
+
+import java.util.List;
+
+public interface PayOrderMapper extends SuperMapper<PayOrder> {
+
+    List<PayOrder> selectOrder(String authCode);
+
+    String selectMinDayOrderAmtCertId(String today);
+}
